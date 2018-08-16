495614b56b1404958198ce2b081f6c337fc219a7
Robert Dahl <33264217+dahlrad333@users.noreply.github.com>
2018-08-16 17:39:01 -0400

    Add files via upload
    now it has the actual files

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = 'ea9c594d7f59764ae12e20caf997fa05'
+
+class App extends React.Component{
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}id=524901&APPID=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    if(city && country){
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: undefined
+      })
+    }
+    else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter values for the city and country fields'
+      })
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
